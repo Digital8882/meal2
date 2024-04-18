2f63e4a666ab15e502c05a3c53eab7f56a753074
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-04-18 13:03:14 +0000

    Update repo.py

--- repo.py
+++ repo.py
@@ -1,9 +1,9 @@
 from langchain_anthropic import ChatAnthropic
 from crewai import Crew, Process, Agent
 from dotenv import load_dotenv
-
-ANTHROPIC_API_KEY="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"
-
+import os
+os.environ["ANTHROPIC_API_KEY"] = "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"
+ 
 Nutritionist = Agent(
     role='Nutritionist',
     goal=f'prescribe healthy meal plan',
